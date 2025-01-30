5066a13f44882159c53862c5b0bc30e20de89655
Ruchira <ruchirakaveesha2020@gmail.com>
2025-01-30 11:27:59 +0530

    list card

---
+++ lib/screens/list_card.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class property_Card extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String price;
+  final String location;
+
+  property_Card(
+      {required this.imageUrl,
+      required this.title,
+      required this.price,
+      required this.location});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      elevation: 4,
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+            child: Image.asset(
+              imageUrl,
+              height: 100,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  location,
+                  style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  price,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.teal.shade600,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
