7d183cfb57ce1dae45da65dfb17d67c541ffee5c
Ruchira <ruchirakaveesha2020@gmail.com>
2025-01-31 11:32:44 +0530

    signup

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -15,6 +15,12 @@ class SignUp extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
       body: Center(
         child: Padding(
@@ -26,7 +32,47 @@ class SignUp extends StatelessWidget {
               Image.asset(
                 "assets/images/main_icon.png",
                 height: 80,
-              )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "Enter Your Full Name Here",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person)),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "Enter Your Email",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email)),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock)),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal.shade600,
+                    padding: EdgeInsets.all(20),
+                  ),
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(color: Colors.white),
+                  ))
             ],
           ),
         ),
