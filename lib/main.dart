e97b395673a611e06201a761f12da28e938038ab
Ruchira <ruchirakaveesha2020@gmail.com>
2025-01-31 12:04:02 +0530

    adding new font

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        fontFamily: 'MavenPro',
+        fontFamily: 'Roboto',
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
